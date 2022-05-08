02350a21fcb29608ad04a1e9f82342641937efca
Sninctbur <hedpub@protonmail.com>
2022-05-08 18:09:02 -0500

    Reworked extended mag for accuracy

--- lua/arccw/shared/attachments/uc_usp_mag_ext.lua
+++ lua/arccw/shared/attachments/uc_usp_mag_ext.lua
@@ -1,13 +1,13 @@
-att.PrintName = "USP 20-Round Extended Mag"
-att.AbbrevName = "20-Round Extended Mag"
+att.PrintName = "USP 17-Round Extended Mag"
+att.AbbrevName = "17-Round Extended Mag"
 
 if !GetConVar("arccw_truenames"):GetBool() then
-    att.PrintName = "P93 20-Round Extended Mag"
+    att.PrintName = "P93 17-Round Extended Mag"
 end
 
 att.SortOrder = 33
 att.Icon = Material("entities/att/acwatt_ud_glock_mag_33.png", "smooth mips")
-att.Description = "Extended magazine with a thickened base for improved ergonomics. Heavier and throws off weapon balance, but does allow you to fire for longer."
+att.Description = "Aftermarket magazine extension with a thickened base for improved ergonomics. Heavier and throws off weapon balance, but does allow you to fire for longer."
 att.Desc_Pros = {}
 att.Desc_Cons = {}
 att.Desc_Neutrals = {}
@@ -17,7 +17,7 @@ att.AutoStats = true
 
 att.Mult_SightTime = 1.1
 att.Mult_ReloadTime = 1.15
-att.Override_ClipSize = 20
+att.Add_ClipSize = 5
 
 att.Mult_HipDispersion = 1.25
 att.Mult_Sway = 1.5
